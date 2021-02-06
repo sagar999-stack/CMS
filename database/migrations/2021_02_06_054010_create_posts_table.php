491208fc110ab86dc2195261a0322429e36d705c
sagar999-stack <fokhrul898@gmail.com>
2021-02-06 12:15:13 +0600

    migrations

---
+++ database/migrations/2021_02_06_054010_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->integer('cms_id')->unsigned();
+            $table->string('title');
+            $table->text('description');
+            $table->string('slug');
+            $table->integer('price');
+            $table->tinyInteger('ststus')->default(0);
+            $table->integer('author_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
